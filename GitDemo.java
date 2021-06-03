50b9df746d985e7d9c2529f6576df661d065bdc8
tchu15 <85319905+tchu15@users.noreply.github.com>
2021-06-03 16:05:52 -0700

    Add files via upload

---
+++ GitDemo.java
@@ -0,0 +1,35 @@
+/*****
+ * Edit this line to have your name.
+ ******/
+public class GitDemo {
+    
+    private double previousNumber;
+    
+    public GitDemo() {
+        previousNumber = 1;
+    }
+    
+    /*****
+     * This function adds the given number to the private "previousNumber"
+     * stored in the GitDemo class. 
+     * 
+     * @param num - A number that should be added to GitDemo:previousNumber
+     * @return the updated value of 'previousNumber'
+    *****/
+    public double addNumber(double num) {
+        System.out.print("Add num to previousNumber");
+        return 0;
+    }
+    
+    /*****
+     * Description of function goes here
+     *
+     * @param - description of passed in arguments go here
+     * @return - description of return value goes here
+     *****/
+    public void templateFunction(int num) {
+        
+        /** Code goes here **/
+        
+    }
+}
